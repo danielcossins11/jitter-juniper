2f82cb46ffff9be7881995ab38111a7334abe8b9
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-18 12:03:46 -0600

    made 3 tests in jot

---
+++ Jitter.Tests/Models/JotTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jitter.Models;
+
+namespace Jitter.Tests.Models
+{
+    [TestClass]
+    public class JotTests
+    {
+        [TestMethod]
+        public void JotEnsureICanCreateInstance()
+        {
+            Jot a_jot = new Jot();
+            Assert.IsNotNull(a_jot);
+        }
+
+        [TestMethod]
+        public void JotEnsureJotHasAllTheThings()
+        {
+            Jot a_jot = new Jot();
+            DateTime expectedTime = DateTime.Now;
+            a_jot.JotId = 1;
+            a_jot.Content = "My Content";
+            a_jot.Date = expectedTime;
+            a_jot.Author = null;
+            a_jot.Picture = "https://";
+
+            Assert.AreEqual(1, a_jot.JotId);
+            Assert.AreEqual("My Content", a_jot.Content);
+            Assert.AreEqual(expectedTime, a_jot.Date);
+            Assert.AreEqual(null, a_jot.Author);
+            Assert.AreEqual("https://", a_jot.Picture);
+        }
+
+        [TestMethod]
+        public void JotEnsureICanUseObjectInitializerSyntax()
+        {
+            DateTime expectedTime = DateTime.Now;
+
+            Jot a_jot = new Jot { JotId = 1, Content = "My Content", Date = expectedTime, Author = null, Picture = "https://" };
+
+            Assert.AreEqual(1, a_jot.JotId);
+            Assert.AreEqual("My Content", a_jot.Content);
+            Assert.AreEqual(expectedTime, a_jot.Date);
+            Assert.AreEqual(null, a_jot.Author);
+            Assert.AreEqual("https://", a_jot.Picture);
+        }
+    }
+}
